2fcab6f567fc13081fa00f2483afd40ede20fa05
tanglin-hub <22419024@zju.edu.cn>
2025-04-18 11:01:34 +0800

    debug:false

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!4gzpwgv(ofl_0nvnxcizgk4$gas##nbckqsx2ivr+o!tbdzuq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['10.71.128.237', '127.0.0.1', 'localhost']
 
