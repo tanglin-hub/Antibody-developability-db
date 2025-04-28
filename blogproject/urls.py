76c35b52344b6ab357bbd5aa776609d120dffc21
tanglin-hub <22419024@zju.edu.cn>
2025-04-28 21:07:28 +0800

    nginx

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls import url
+from django.views import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', include('blog.urls')),
+    url(r'static/(?P<path>.*)$',static.serve,
+        {'document_root':settings.STATIC_ROOT},name='static'),
 ]
